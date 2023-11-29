8d3a0b733e3457ba6b11ae975f492938ed314e43
muratcankargi <muratcankargi@hotmail.com>
2023-11-29 20:46:11 +0300

    add switch button and router push operation

--- components/BiletSatis.vue
+++ components/BiletSatis.vue
@@ -11,6 +11,12 @@
         </select>
       </div>
 
+      <div class="mb-4 flex justify-center">
+        <button @click="switchCities" class="bg-blue-500 text-white p-2 rounded-md">
+          <svg fill="#131717" width="20" height="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M6.99 11L3 15l3.99 4v-3H14v-2H6.99v-3zM21 9l-3.99-4v3H10v2h7.01v3L21 9z"></path></svg>
+        </button>
+      </div>
+
       <div class="mb-4">
         <label for="to" class="block text-sm font-medium text-gray-600">Nereye:</label>
         <select v-model="to" id="to" name="to" class="mt-1 p-2 border border-gray-300 rounded-md w-full">
@@ -31,7 +37,7 @@
 
 <script>
 export default {
-  mounted(){
+  mounted() {
     this.from = 'İstanbul';
     this.to = 'Ankara';
     this.date = this.today;
@@ -42,20 +48,25 @@ export default {
       from: '',
       to: '',
       date: '',
-      cities: ['İstanbul', 'Ankara', 'İzmir', 'Antalya', 'Bursa'] ,
+      cities: ['İstanbul', 'Ankara', 'İzmir', 'Antalya', 'Bursa'],
       disabledDates: {
         to: new Date().toISOString().split('T')[0]
-      },
+      }
     };
   },
   methods: {
     buyTicket() {
       console.log('Bilet Satın Alındı!');
+      this.$router.push('/seferler');
+    },
+    switchCities() {
+      const temp = this.from;
+      this.from = this.to;
+      this.to = temp;
     }
   }
 };
 </script>
 
 <style scoped>
-/* Bileşen özel stillemeleri burada yapılabilir */
 </style>
