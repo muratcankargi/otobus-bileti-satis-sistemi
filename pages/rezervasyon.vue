42407739ac5935b7c002a525cd7183dd32026837
muratcankargi <muratcankargi@hotmail.com>
2023-11-30 23:21:11 +0300

    Create rezervasyon.vue

---
+++ pages/rezervasyon.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <Rezervasyon/>
+    </div>
+</template>
+<script>
+import Rezervasyon from '../components/Rezervasyon'
+export default {
+  components: {
+    Rezervasyon
+  }
+  }
+</script>
