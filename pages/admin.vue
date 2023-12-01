bce56892d6769eb081159d1d52e1bad3b5a66345
muratcankargi <muratcankargi@hotmail.com>
2023-12-01 19:16:48 +0300

    Create admin.vue

---
+++ pages/admin.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <Admin/>
+    </div>
+</template>
+<script>
+import Admin from '../components/Admin'
+export default {
+  components: {
+    Admin
+  }
+  }
+</script>
