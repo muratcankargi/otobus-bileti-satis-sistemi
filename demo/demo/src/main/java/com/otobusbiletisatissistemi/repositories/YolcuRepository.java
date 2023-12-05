a781d955a442a55c8aaa3ce79e9d3db24c975418
muratcankargi <muratcankargi@hotmail.com>
2023-12-05 23:51:13 +0300

    Update name and Add email operation

--- demo/demo/src/main/java/com/otobusbiletisatissistemi/repositories/YolcuRepository.java
+++ demo/demo/src/main/java/com/otobusbiletisatissistemi/repositories/YolcuRepository.java
@@ -1,9 +1,14 @@
 package com.otobusbiletisatissistemi.repositories;
-import com.otobusbiletisatissistemi.entities.Yolcu;
+import com.otobusbiletisatissistemi.entities.Yolcular;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface YolcuRepository extends JpaRepository<Yolcu, Long> {
+public interface YolcuRepository extends JpaRepository<Yolcular, Long> {
+    @Query("SELECT s FROM Yolcular s WHERE s.email = ?1")
+    Optional<Yolcular> findYolcularByEmail(String email);
 }
 
