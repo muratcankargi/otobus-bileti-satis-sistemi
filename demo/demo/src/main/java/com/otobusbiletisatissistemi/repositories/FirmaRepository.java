2aa4351709ee0f3fb3a3d1d8ad5ffbd3246ae109
muratcankargi <muratcankargi@hotmail.com>
2023-12-06 22:31:03 +0300

    Add jpql query

--- demo/demo/src/main/java/com/otobusbiletisatissistemi/repositories/FirmaRepository.java
+++ demo/demo/src/main/java/com/otobusbiletisatissistemi/repositories/FirmaRepository.java
@@ -2,9 +2,14 @@ package com.otobusbiletisatissistemi.repositories;
 
 import com.otobusbiletisatissistemi.entities.Firmalar;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface FirmaRepository extends JpaRepository<Firmalar, Long> {
 
+    @Query("SELECT s FROM Firmalar s WHERE s.firmaIletisim = ?1")
+    Optional<Firmalar> findFirmalarByFirmaIletisim(String email);
 }
