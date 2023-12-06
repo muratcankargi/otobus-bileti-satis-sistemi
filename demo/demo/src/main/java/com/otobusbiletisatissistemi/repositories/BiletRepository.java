a6bed9457d07d6edd11232f2254f7aed08955329
muratcankargi <muratcankargi@hotmail.com>
2023-12-06 22:25:52 +0300

    Add jpql query

--- demo/demo/src/main/java/com/otobusbiletisatissistemi/repositories/BiletRepository.java
+++ demo/demo/src/main/java/com/otobusbiletisatissistemi/repositories/BiletRepository.java
@@ -1,7 +1,14 @@
 package com.otobusbiletisatissistemi.repositories;
 
 import com.otobusbiletisatissistemi.entities.Biletler;
+import com.otobusbiletisatissistemi.entities.Seferler;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BiletRepository extends JpaRepository<Biletler, Long> {
+    //1 seferde yalnızca 1 koltuğu birisi alabilir
+    @Query("SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END FROM Biletler b WHERE" +
+            " b.seferId = :seferId AND b.KoltukNo = :koltukNo")
+    boolean existsBySeferIdAndKoltukNo(Long seferId, Integer koltukNo);
+
 }
