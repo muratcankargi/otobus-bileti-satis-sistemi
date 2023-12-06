481a15ee5875639b8a5c216c4b7a87aea7db9cc3
muratcankargi <muratcankargi@hotmail.com>
2023-12-06 22:31:15 +0300

    Add jpql query

--- demo/demo/src/main/java/com/otobusbiletisatissistemi/repositories/OtobusRepository.java
+++ demo/demo/src/main/java/com/otobusbiletisatissistemi/repositories/OtobusRepository.java
@@ -2,6 +2,11 @@ package com.otobusbiletisatissistemi.repositories;
 
 import com.otobusbiletisatissistemi.entities.Otobusler;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface OtobusRepository extends JpaRepository<Otobusler, Long> {
+    @Query("SELECT s FROM Otobusler s WHERE s.otobusPlaka = ?1")
+    Optional<Otobusler> findOtobuslerByOtobusPlaka(String plaka);
 }
