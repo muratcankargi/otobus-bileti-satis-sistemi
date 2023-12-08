612c223436a89e768bdd2fc6e922030d59607cae
muratcankargi <muratcankargi@hotmail.com>
2023-12-08 14:55:17 +0300

    data extraction with api

--- components/BiletAra.vue
+++ components/BiletAra.vue
@@ -1,46 +1,45 @@
 <template>
-  <div class="flex items-center justify-center h-screen">
-    <div class="w-full max-w-md p-4">
-      <h1 class="text-2xl font-bold mb-4">Şehirler Arası Otobüs Bilet Sistemi</h1>
-      
-      <div class="mb-4">
-        <label for="from" class="block text-sm font-medium text-gray-600">Nereden:</label>
-        <select v-model="from" id="from" name="from" class="mt-1 p-2 border border-gray-300 rounded-md w-full">
+  <div class="flex items-start justify-start h-screen p-4">
+    <div class="w-full max-w-md">
+      <div class="mb-4 flex items-center">
+        <label for="from" class="mr-2 text-sm font-medium text-gray-600">Nereden:</label>
+        <select v-model="from" id="from" name="from" class="p-2 border border-gray-300 rounded-md">
           <option value="" disabled selected>Lütfen seçiniz</option>
-          <option v-for="city in cities" :key="city" :value="city">{{ city }}</option>
+          <option v-for="city in fromCities" :key="city" :value="city">{{ city.Sehir }}</option>
         </select>
       </div>
 
-      <div class="mb-4 flex justify-center">
-        <button @click="switchCities" class="bg-blue-500 text-white p-2 rounded-md">
-          <svg fill="#131717" width="20" height="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M6.99 11L3 15l3.99 4v-3H14v-2H6.99v-3zM21 9l-3.99-4v3H10v2h7.01v3L21 9z"></path></svg>
-        </button>
-      </div>
-
-      <div class="mb-4">
-        <label for="to" class="block text-sm font-medium text-gray-600">Nereye:</label>
-        <select v-model="to" id="to" name="to" class="mt-1 p-2 border border-gray-300 rounded-md w-full">
+      <div class="flex items-center mb-4">
+        <label for="to" class="mr-2 text-sm font-medium text-gray-600">Nereye:</label>
+        <select v-model="to" id="to" name="to" class="p-2 border border-gray-300 rounded-md">
           <option value="" disabled selected>Lütfen seçiniz</option>
-          <option v-for="city in cities" :key="city" :value="city">{{ city }}</option>
+          <option v-for="city in toCities" :key="city" :value="city">{{ city.Sehir }}</option>
         </select>
       </div>
 
-      <div class="mb-4">
-        <label for="date" class="block text-sm font-medium text-gray-600">Tarih:</label>
-        <input v-model="date" type="date" id="date" name="date" class="mt-1 p-2 border border-gray-300 rounded-md w-full" :min="today" :disabledDates="disabledDates">
+      <div class="flex items-center mb-4">
+        <label for="date" class="mr-2 text-sm font-medium text-gray-600">Tarih:</label>
+        <input v-model="date" type="date" id="date" name="date" class="p-2 border border-gray-300 rounded-md" 
+        :min="today" :disabledDates="disabledDates">
       </div>
 
-      <button @click="searchTicket" class="bg-blue-500 text-white p-2 rounded-md w-full">Bilet Ara</button>
+      <div class="flex items-center">
+        <button @click="searchTicket" class="bg-blue-500 text-white p-2 rounded-md">Bilet Ara</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   mounted() {
+    this.fetchData();
     this.from = 'İstanbul';
     this.to = 'Ankara';
     this.date = this.today;
+
   },
   data() {
     return {
@@ -48,22 +47,50 @@ export default {
       from: '',
       to: '',
       date: '',
-      cities: ['İstanbul', 'Ankara', 'İzmir', 'Antalya', 'Bursa'],
+      fromCities: [],
+      toCities:[],
       disabledDates: {
         to: new Date().toISOString().split('T')[0]
       }
     };
   },
   methods: {
+    async fetchData() {
+      await axios.get('http://localhost:8080/api/seferler/nereden')
+        .then(response => {
+          this.fromCities = response.data;
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+
+        await axios.get('http://localhost:8080/api/seferler/nereye')
+        .then(response => {
+          this.toCities = response.data;
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+    },
     searchTicket() {
-      console.log('Bilet Satın Alındı!');
-      this.$router.push('/seferler');
+      const postData = {
+          from: this.from,
+          to: this.to,
+          date: this.date,
+        };
+
+        this.$router.push({
+        name: 'seferler',
+        params: postData,
+      });
+
     },
     switchCities() {
       const temp = this.from;
       this.from = this.to;
       this.to = temp;
-    }
+    },
+    
   }
 };
 </script>
--- components/Seferler.vue
+++ components/Seferler.vue
@@ -1,50 +1,63 @@
 <template>
-    <div class="relative overflow-x-auto shadow-md">
-      <table class="w-full text-sm text-left">
-        <thead class="text-xs uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" class="px-6 py-3">Firma</th>
-            <th scope="col" class="px-6 py-3">Nereden</th>
-            <th scope="col" class="px-6 py-3">Nereye</th>
-            <th scope="col" class="px-6 py-3">Sefer Tarihi</th>
-            <th scope="col" class="px-6 py-3"></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(data, index) in dataTable" :key="index" class="border-b dark:border-gray-700 hover:bg-gray-100">
-            <td scope="row" class="px-6 py-4">{{ data.firma }}</td>
-            <td class="px-6 py-4">{{ data.from }}</td>
-            <td class="px-6 py-4">{{ data.to }}</td>
-            <td class="px-6 py-4">{{data.date }}</td>
-            <td class="px-6 py-4 text-right">
-              <router-link class="font-medium text-blue-600 dark:text-blue-500 hover:underline" :to="{ path: '/rezervasyon'}">Bilet Al</router-link>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    mounted() {
-     },
-    data() {
-      return {
-        dataTable: [
-          { firma: 'Zafer Turizm', from: 'İstanbul', to: "Suriye", date: '2023-11-28T12:34:56'},
-          { firma: 'Lüks Artvin', from: 'Ankara', to: "İzmir", date: '2023-11-27T09:45:23'},
-        ],
-        input_data: "",
-      };
+  <div class="relative overflow-x-auto shadow-md">
+    <table class="w-full text-sm text-left">
+      <thead class="text-xs uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+          <th scope="col" class="px-6 py-3">Firma</th>
+          <th scope="col" class="px-6 py-3">Nereden</th>
+          <th scope="col" class="px-6 py-3">Nereye</th>
+          <th scope="col" class="px-10 py-3">Sefer Tarihi</th>
+          <th scope="col" class="px-10 py-3">Varis Tarihi</th>
+          <th scope="col" class="px-10 py-3"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(data, index) in dataTable" :key="index" class="border-b dark:border-gray-700 hover:bg-gray-100">
+          <td scope="row" class="px-6 py-4">{{ data.firma.firmaAdi }}</td>
+          <td class="px-6 py-4">{{ data.seferKalkisYeri }}</td>
+          <td class="px-6 py-4">{{ data.seferVarisYeri }}</td>
+          <td class="px-6 py-4">{{ formatDate(data.seferKalkisSaati) }}</td>
+          <td class="px-6 py-4">{{ formatDate(data.seferVarisSaati) }}</td>
+          <td class="px-6 py-4 text-right">
+            <router-link class="font-medium text-blue-600 dark:text-blue-500 hover:underline" :to="{ path: '/rezervasyon'}">Bilet Al</router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  mounted() {
+    this.fetchData();
+   console.log(this.$route.params);
+  },
+  data() {
+    return {
+      dataTable: [],
+      input_data: "",
+    };
+  },
+  methods: {
+    async fetchData() {
+      await axios.get('http://localhost:8080/api/seferler')
+        .then(response => {
+          this.dataTable = response.data;
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
     },
-    methods: {
+    formatDate(dateTimeString) {
+      const options = { day: 'numeric', month: 'long', hour: 'numeric', minute: 'numeric' };
+      const formattedDate = new Date(dateTimeString).toLocaleDateString('tr-TR', options);
+      return formattedDate;
     },
-    
-  };
-  </script>
-  
-  <style scoped>
-  
-  </style>
-  
+  },
+};
+</script>
+
+<style scoped>
+</style>
