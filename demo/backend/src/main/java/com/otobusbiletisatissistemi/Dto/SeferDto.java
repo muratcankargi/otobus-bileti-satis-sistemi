e6fd5bf013f5d3855ad2ad46c28b3a33e9f45dba
muratcankargi <muratcankargi@hotmail.com>
2024-02-10 19:43:49 +0300

    Update SeferDto.java

--- demo/backend/src/main/java/com/otobusbiletisatissistemi/Dto/SeferDto.java
+++ demo/backend/src/main/java/com/otobusbiletisatissistemi/Dto/SeferDto.java
@@ -1,8 +1,12 @@
 package com.otobusbiletisatissistemi.Dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
-// SeferDto.java
+@Getter
+@Setter
 public class SeferDto {
     private Long firmaId;
     private Long otobusId;
@@ -12,53 +16,5 @@ public class SeferDto {
     private LocalDateTime seferVarisSaati;
 
     public SeferDto() {
-    }
-
-    public Long getFirmaId() {
-        return firmaId;
-    }
-
-    public void setFirmaId(Long firmaId) {
-        this.firmaId = firmaId;
-    }
-
-    public Long getOtobusId() {
-        return otobusId;
-    }
-
-    public void setOtobusId(Long otobusId) {
-        this.otobusId = otobusId;
-    }
-
-    public String getSeferKalkisYeri() {
-        return seferKalkisYeri;
-    }
-
-    public void setSeferKalkisYeri(String seferKalkisYeri) {
-        this.seferKalkisYeri = seferKalkisYeri;
-    }
-
-    public String getSeferVarisYeri() {
-        return seferVarisYeri;
-    }
-
-    public void setSeferVarisYeri(String seferVarisYeri) {
-        this.seferVarisYeri = seferVarisYeri;
-    }
-
-    public LocalDateTime getSeferKalkisSaati() {
-        return seferKalkisSaati;
-    }
-
-    public void setSeferKalkisSaati(LocalDateTime seferKalkisSaati) {
-        this.seferKalkisSaati = seferKalkisSaati;
-    }
-
-    public LocalDateTime getSeferVarisSaati() {
-        return seferVarisSaati;
-    }
-
-    public void setSeferVarisSaati(LocalDateTime seferVarisSaati) {
-        this.seferVarisSaati = seferVarisSaati;
     }
 }
